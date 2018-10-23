76106b905deda2388e006ad353c96767085d9542
xclovelyl <393398862@qq.com>
2018-10-23 09:37:38 +0800

    解决导入MUIjs文件后产生的问题

--- src/components/photos/PhotoList.vue
+++ src/components/photos/PhotoList.vue
@@ -23,11 +23,17 @@
 </template>
 
 <script>
+import mui from "../../assets/mui/js/mui.min.js"
 export default {
-  
+  mounted() {
+  // 需要在组件的 mounted 事件钩子中，注册 mui 的 scroll 滚动事件
+     mui(".mui-scroll-wrapper").scroll({
+       deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+     });
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+    * { touch-action: pan-y; }
 </style>
